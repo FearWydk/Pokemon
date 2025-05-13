ac4db6732dfc5ceffe1ee0200fa2ccd02d560483
FearWydk <brooks.joshua43@gmail.com>
2025-05-13 18:56:08 -0400

    Assignment - Create main() function
    Created a Main function with cout and cin function.

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,6 +1,19 @@
-#include <iostream>
+#include<iostream>
+using namespace std;
 
-int main() {
+int main()
+{
 
-    return 0;
-}
+	string PlayerName;
+
+	cout << "Trainer!Before you embark on your journey,the Pokémon world needs to know who you are" << endl;	//1st message from Professor Oak
+	cout <<"Time to step up and tell us your name and remember, a great name is the start of every great adventure!" << endl <<endl;	//2nd message from Professor Oak
+
+	cout << "Enter your name" << endl;	//Enter Player Name Message
+
+	cin >> PlayerName;	//Player Input Name
+
+	cout << endl << "Oh your name is " << PlayerName << ", Welcome to the world of Pokemon!" << endl;	//Finished statment
+
+
+};
